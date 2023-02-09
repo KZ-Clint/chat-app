1f0af0fc79ef0df43218fce6468cb0fa2da9a7cc
KZ-Clint <ezeclinton3@gmail.com>
2023-02-09 19:01:40 +0000

    third commit

--- components/base/baseUrl.js
+++ components/base/baseUrl.js
@@ -1,5 +1,5 @@
 export default process.env.NODE_ENV === "production"
-  ? "https://rainbow-fairy-61e207.netlify.app" 
+  ? "https://chat-app-backend-5u74.onrender.com"
   :    "http://192.168.43.95:5000"
   
   
